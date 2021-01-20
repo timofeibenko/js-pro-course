5a6e3898725673b61cc9c934b9f5edefc87b93a6
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-01-20 23:46:10 +0300

    fix: set methods to prototype, rewrite arrow function to regular

--- js-classes/inheritance-es5.js
+++ js-classes/inheritance-es5.js
@@ -1,4 +1,4 @@
-// ES5
+//--------- ES5----------
 
 function Car(name, model, year, color, maxSpeed, fuelCapacity = 60, fuelConsumption = 10) {
     this.name = name;
@@ -8,77 +8,103 @@ function Car(name, model, year, color, maxSpeed, fuelCapacity = 60, fuelConsumpt
     this.maxSpeed = maxSpeed;
     this.fuelCapacity = fuelCapacity;
     this.fuelConsumption = fuelConsumption;
+};
 
-    this.getFullName = function() {
-        return `${this.name} ${this.model}`;
-    };
+// Car methods
 
-    this.getAge = function() {
-        var date = new Date();
-        return date.getFullYear() - this.year;
-    };
+Car.prototype.getFullName = function() {
+    return `${this.name} ${this.model}`;
+};
 
-    this.changeColor = function(color) {
-        color === this.color ? 
-        alert(`The car is already painted ${this.color}.`) 
-        : this.color = color
-    };
+Car.prototype.getAge = function() {
+    let date = new Date();
+    return date.getFullYear() - this.year;
+};
 
-    this.calculateWay = function(kilometers, fuel) {
-        if (fuel < 10) {
-            alert('Fuel is less than 10');
+Car.prototype.changeColor = function(color) {
+    if ( color === this.color ) {
+        alert(`The car is already painted ${this.color}.`);
+        return;
+    }
+    
+    this.color = color;
+};
+
+Car.prototype.calculateWay = function(kilometers, fuel) {
+    if (fuel < 10) {
+        alert('Fuel is less than 10');
+    } else {
+        let time = kilometers / this.maxSpeed;
+        let fuelNeeded = time * this.fuelConsumption
+
+        if(fuelNeeded > fuel) {
+            alert(`You'll have to get additional ${fuelNeeded - fuel} units of fuel to get there!`);
         } else {
-            var time = kilometers / this.maxSpeed;
-            var fuelNeeded = time * this.fuelConsumption
-
-            fuelNeeded > fuel ? 
-            alert(`You'll have to get additional ${fuelNeeded - fuel} units of fuel to get there!`) :
-            alert(`You dont't need extra fuel to get there!`)
-
-            return time
+            alert(`You dont't need extra fuel to get there!`);
         }
-    };
+
+        return time
+    }
 };
 
-function BMW(name, model, year, color, maxSpeed, fuelCapacity = 60, fuelConsumption = 10) {
+// BMW
+
+function BMW(name, model, year, color, maxSpeed, climateControl, fuelCapacity = 60, fuelConsumption = 10) {
+
     Car.call(this, name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption);
 
-    this.climateControl = true;
-
-    this.isUpdateRequred = function() {
-        return this.getFullYear() > 10 ? true : false;
-    };
+    this.climateControl = climateControl;
 };
 
-function Lada(name, model, year, color, maxSpeed, fuelCapacity = 60, fuelConsumption = 10) {
+BMW.prototype = Car.prototype;
+
+BMW.prototype.isUpdateRequred = function() {
+    return this.getAge() > 10 ? true : false;
+};
+
+// Lada
+
+function Lada(name, model, year, color, maxSpeed, tonirovochka,  fuelCapacity = 60, fuelConsumption = 10) {
     Car.call(this, name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption);
 
-    this.tonirovochka = true;
+    this.tonirovochka = tonirovochka;
+};
 
-    this.isLastochka = function() {
-        return this.model === 'Priora' ? alert('Of course, bro') : alert('Nie');
+Lada.prototype.isLastochka = function() {
+    if (this.model === 'Priora') {
+        return true;
+    } else {
+        return false;
     };
 };
+
+// Lexus
 
 function Lexus(name, model, year, color, maxSpeed, configuration, fuelCapacity = 60, fuelConsumption = 10) {
     Car.call(this, name, model, year, color, maxSpeed, fuelCapacity, fuelConsumption);
     
     this.configuration = configuration;
-    
-    this.getSeatsNumber = function() {
-        if (this.configuration === 'sedan') {
-            return 5;
-        } else if (this.configuration === 'hatchback') {
-            return 2;
-        }
-    };
 };
 
-var car_BMW_ES5 = new BMW('X5', 'deluxe', 2005, 'purple', 200);
-var car_lada_ES5 = new Lada('Priora', 'LUHURI', 2010, 'baklajan', 150);
-var car_lexus_ES5 = new Lexus('smth', 'idk', 2015, 'pink', 250, 'sedan');
+Lexus.prototype.getSeatsNumber = function() {
+    const configurations = {
+        sedan: 5,
+        hatchback: 2,
+    }
+    
+    return configurations[this.configuration]
+};
 
-// ES 6
+// ES5 entities
+
+const car_BMW_ES5 = new BMW('X5', 'deluxe', 2005, 'purple', 200);
+const car_lada_ES5 = new Lada('Priora', 'LUHURI', 2010, 'baklajan', 150);
+const car_lexus_ES5 = new Lexus('smth', 'idk', 2015, 'pink', 250, 'sedan');
+
+console.log();
+console.log(car_lexus_ES5.getSeatsNumber());
+
+//------------ES 6--------------
 
 class CarES6 {
     constructor(name, model, year, color, maxSpeed, configuration, fuelCapacity = 60, fuelConsumption = 10) {
@@ -91,29 +117,33 @@ class CarES6 {
         this.fuelConsumption = fuelConsumption;
     }
 
-    getFullName = () => `${this.name} ${this.model}`;
+    getFullName() {
+        `${this.name} ${this.model}`
+    };
 
-    getAge = () => {
+    getAge() {
         const date = new Date();
         return date.getFullYear() - this.year;
     };
     
-    changeColor = (color) => {
+    changeColor(color) {
         color === this.color ? 
         alert(`The car is already painted ${this.color}.`) 
         : this.color = color
     };
 
-    calculateWay = (kilometers, fuel) => {
+    calculateWay(kilometers, fuel) {
         if (fuel < 10) {
             alert('Fuel is less than 10');
         } else {
-            var time = kilometers / this.maxSpeed;
-            var fuelNeeded = time * this.fuelConsumption
+            const time = kilometers / this.maxSpeed;
+            const fuelNeeded = time * this.fuelConsumption
 
-            fuelNeeded > fuel ? 
-            alert(`You'll have to get additional ${fuelNeeded - fuel} units of fuel to get there!`) :
-            alert(`You dont't need extra fuel to get there!`)
+            if (fuelNeeded > fuel) {
+                alert(`You'll have to get additional ${fuelNeeded - fuel} units of fuel to get there!`);
+            } else {
+                alert(`You dont't need extra fuel to get there!`)
+            }
 
             return time
         }
@@ -127,7 +157,9 @@ class BMWES6 extends CarES6 {
         this.climateControl = true;
     };
 
-    isUpdateRequred = () => this.getFullYear() > 10 ? true : false;
+    isUpdateRequred() {
+        return this.getFullYear() > 10 ? true : false;
+    }
 };
 
 class LadaES6 extends CarES6 {
@@ -137,7 +169,9 @@ class LadaES6 extends CarES6 {
         this.tonirovochka = true;
     };
 
-    isLastochka = () => this.model === 'Priora' ? alert('Of course, bro') : alert('Nie');
+    isLastochka() {
+        this.model === 'Priora' ? true : false
+    };
 };
 
 class LexusES6 extends CarES6 {
