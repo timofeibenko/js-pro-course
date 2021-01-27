583c860fc39de378c9d3a056565f1f8413376075
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-01-27 22:28:58 +0300

    refactor: change My_Math factory to produce an ES5 class

--- math-function/My_Math.js
+++ math-function/My_Math.js
@@ -1,78 +1,71 @@
 (function (window) {
     function My_Math(init) {
-        const getInitValue = () => init;
-    
-        class Math {
-            constructor() {
-                this.init = getInitValue();
-            };
-    
-            plus(val) {
-                this.init += val
-                return this
-            };
-    
-            minus(val) {
-                this.init -= val
-                return this
-            };
-    
-            divide(val) {
-                if (val === 0) {
-                    this.init /= 1
-                } else {
-                    this.init /= val
-                }
-                return this
-            };
-    
-            multi(val) {
-                this.init *= val
-                return this
-            };
-    
-            value() {
-                return this.init
-            };
-    
-            revert() {
-                this.init *= -1
-                return this
-            };
-    
-            pow(val) {
-                this.init = this.init ** val
-                return this
-            };
-    
-            factorial() {
-                if (this.init <= 0) {
-                    this.init = 1
-                    return this
-                };
-    
-                let arr = [];
-                let firstNum = this.init;
-    
-                while (firstNum > 0) {
-                    arr.push(firstNum)
-                    firstNum--
-                };
-    
-                this.init = arr.reduce((a, b) => a * b)
-                return this
-            };
-    
-            format(callback) {
-                return callback(this.init)  
-            };
+
+        function Calculator() {
+            this.init = init;
         };
-
-        return new Math
-    };
-    console.log(Math.pow(5, 2));
-
-    let result = My_Math(5).pow(2).value()
-    console.log(result);
-
+    
+        Calculator.prototype.plus = function(val) {
+            this.init += val
+            return this
+        };
+    
+        Calculator.prototype.minus = function(val) {
+            this.init -= val
+            return this
+        };
+    
+    
+        Calculator.prototype.divide = function(val) {
+            if (val === 0) {
+                this.init /= 1
+            } else {
+                this.init /= val
+            }
+            return this
+        };
+    
+        Calculator.prototype.multi = function(val) {
+            this.init *= val
+            return this
+        };
+    
+        Calculator.prototype.value = function() {
+            return this.init
+        };
+    
+        Calculator.prototype.revert = function() {
+            this.init *= -1
+            return this
+        };
+    
+        Calculator.prototype.pow = function (val) {
+            this.init = this.init ** val
+            return this
+        };
+    
+        Calculator.prototype.factorial = function() {
+            if (this.init <= 0) {
+                this.init = 1
+                return this
+            };
+    
+            let arr = [];
+            let firstNum = this.init;
+    
+            while (firstNum > 0) {
+                arr.push(firstNum)
+                firstNum--
+            };
+    
+            this.init = arr.reduce((a, b) => a * b)
+            return this
+        };
+    
+        Calculator.prototype.format = function (callback) {
+            return callback(this.init)  
+        };
+    
+        return new Calculator
+    };    
 })(window);
